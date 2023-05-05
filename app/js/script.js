6381badf54fb9ed9745bb681aa05df0f50cf3714
michaelshodge <michaelshodge@gmail.com>
2023-05-05 10:17:34 -0500

    Big update

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,36 @@
-console.log("hello");
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+
+const setDarkMode = () => {
+    (document.querySelector('body').classList = 'dark');
+    localStorage.setItem('colorMode', 'dark');
+};
+
+const setLightMode = () => {
+    (document.querySelector('body').classList = 'light');
+    localStorage.setItem('colorMode', 'light');
+};
+
+const colorModeFromLocalStorage = () => {
+    return localStorage.getItem('colorMode');
+  };
+
+  const colorModeFromPreferences = () => {
+    return window.matchMedia('(prefers-color-scheme: dark)').matches 
+                ? 'dark'
+                : 'light' 
+  };
+
+  const loadAndUpdateColor = () => {
+    const color = colorModeFromLocalStorage() || colorModeFromPreferences();
+    color == 'dark' ? darkButton.click() : lightButton.click();
+  };
+
+  const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+    radioButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            darkButton.checked ? setDarkMode() : setLightMode();
+     });
+    });
+
+    loadAndUpdateColor();
